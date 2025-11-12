b12115c96b302140598f6bdb48c337186fff94ce
Kriol368 <kriol368@gmail.com>
2025-11-12 18:50:52 +0100

    updated name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,6 @@ import java.util.Scanner
 import com.mongodb.client.MongoClients
 
 
-
 const val NOM_SRV = "mongodb://root:Taller2014@localhost:27017"
 const val NOM_BD = "tiendainstrumentos"
 const val NOM_COLECCION = "instrumento"
@@ -12,10 +11,10 @@ const val NOM_COLECCION = "instrumento"
 val scanner = Scanner(System.`in`)
 
 fun main() {
-    mostrarPlantas()
+    mostrarInstrumentos()
 }
 
-fun mostrarPlantas() {
+fun mostrarInstrumentos() {
     val cliente = MongoClients.create(NOM_SRV)
     val db = cliente.getDatabase(NOM_BD)
     val coleccion = db.getCollection(NOM_COLECCION)
